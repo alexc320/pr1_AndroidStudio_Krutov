ba9457a431dbd31e1a8d3f57bf3954877d1c2322
2769-23 <krutovlex@gmail.com>
2026-01-24 13:25:42 +0300

    Add binding, configured constrait layout

--- app/src/main/java/com/example/pr1_androidstudio_krutov/MainActivity.kt
+++ app/src/main/java/com/example/pr1_androidstudio_krutov/MainActivity.kt
@@ -8,23 +8,33 @@ import androidx.core.view.WindowInsetsCompat
 import android.widget.Button
 import android.widget.Toast
 import  android.widget.TextView
+import com.example.pr1_androidstudio_krutov.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var  binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom)
             insets
         }
-        val button = findViewById<Button>(R.id.button)
-        val resultText = findViewById<TextView>(R.id.result)
-        button.setOnClickListener {
-            val randomValue = Random.nextInt(1, 7)
-            resultText.text = randomValue.toString()
+            binding.button.setOnClickListener {
+                val randomValue = Random.nextInt(from = 1, until = 7)
+                binding.result.text = randomValue.toString()
+            }
         }
     }
-}
